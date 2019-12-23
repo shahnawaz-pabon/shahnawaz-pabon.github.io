4938ec582a49a5c608c6870ddd694feb5d66b589
Shahnawaz Hossan <s.pabon93@gmail.com>
2019-12-24 00:27:31 +0600

    Gatsby site deployed from master

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/getting-started-with-gatsby",
   siteMetadata: {
     title: `Shahnawaz Pabon - Software Engineer`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
