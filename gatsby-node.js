38aad8d4766ea3d2d196ddcd540fb54465a5738d
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-01-22 23:41:26 +0600

    Gatsby node file added with creating slug

---
+++ gatsby-node.js
@@ -0,0 +1,15 @@
+const path = require(`path`);
+
+const { createFilePath } = require(`gatsby-source-filesystem`);
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
