aa0ff74b85e506cbb703d1fcda6df6cc05c39d8c
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-02-03 23:48:13 +0600

    Container added on navigation bar

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -3,6 +3,27 @@ import { Link } from 'gatsby';
 import book_reading from '../../../static/logos/book-reader.png';
 import './navbar.css';
 
+import styled from 'styled-components';
+
+import ThemeLight from '../../themes/ThemeLight';
+import ThemeDark from '../../themes/ThemeDark';
+
+import Icon from '../StyleSwitch';
+
+const Container = styled.div`
+  max-width: 100vw;
+  height: 17vh;
+  margin: 0 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${props => props.theme.colors.background};
+  transition: all 0.5s ease-out;
+  @media only screen and (max-width: 768px) {
+    margin: 0 2rem;
+  }
+`
+
 export default class Navigation extends Component {
 
   render() {
