e0a93ca21c7ee1ffd7b48d6dbf8148591f2b8eb9
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-02-07 00:41:28 +0600

    Bulb's color fixed on toggle

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,23 +1,15 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import NavigationBar from '../components/NavigationBar';
 import config from '../data/config';
 import '../styles/global.css';
-import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
 import ThemeLight from '../themes/ThemeLight';
 import ThemeDark from '../themes/ThemeDark';
 
 import StyleSwitch from '../components/StyleSwitch';
-
-const Container = styled.div`
-  min-height: 100vh;
-  background-color: ${props => props.theme.colors.background};
-  color: ${props => props.theme.colors.textColor};
-  transition: all 0.5s ease-out;
-`
 
 const Layout = ({ children }) => {
 
@@ -54,10 +46,6 @@ const Layout = ({ children }) => {
 
   )
   
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
