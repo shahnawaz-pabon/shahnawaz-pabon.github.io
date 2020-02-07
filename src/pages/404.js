1f2366c11182e66ebbd10a3101a25d028eef936b
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-02-08 00:33:26 +0600

    Lottie web added on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,31 @@
 import React from "react";
 import Layout from '../layout';
+import lottie from 'lottie-web';
+import animationData from '../../static/assets/lottie/404.json';
 import '../styles/404.css';
 
 class NotFound extends React.Component {
-    render(){
-       return (
-           <Layout>
-             <div className="container">
-                <h1 className="about-header">404 Not Found</h1>
-             </div>
-           </Layout>
-       )
-    }
+
+  componentDidMount(){
+
+    lottie.loadAnimation({
+      container: document.getElementById('not-found'), // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData
+    })
+  }
+
+  render(){
+    return (
+        <Layout>
+          <div className="container">
+            <div id="not-found"></div>
+          </div>
+        </Layout>
+    )
+  }
 }
 
 export default NotFound;
