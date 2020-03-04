15e42d267eb39e3dfe7da71a4520fb8cdf278168
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-03-04 23:44:32 +0600

    Sidebar removed from component and add it to posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -2,10 +2,28 @@ import React from "react";
 import Layout from '../layout';
 import Helmet from 'react-helmet';
 import config from '../data/config';
-import SideBar from '../components/SideBar';
 import PostListing from '../components/PostListing';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHandPointRight } from "@fortawesome/free-solid-svg-icons";
+import '../styles/sidebar.css';
 
 class Posts extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  handleClick(categoryName) {
+    console.log("Category Clicked");
+    console.log(categoryName);
+
+    let filteredCategory = this.props.data.articles.edges.filter(edge =>
+      edge.node.frontmatter.category[0].toLowerCase().includes(categoryName.toLowerCase())
+    );
+
+    console.log("filteredCategory");
+    console.log(filteredCategory);
+  }
+
   render() {
     const postCategories = this.props.data.categories.group;
     const postEdges = this.props.data.articles.edges;
@@ -18,7 +36,41 @@ class Posts extends React.Component {
           <section>
             <PostListing postEdges={postEdges} />
           </section>
-          <SideBar postEdges={postEdges} postCategories={postCategories} />
+
+          {/* Sidebar */}
+          <div className="sidebar">
+            <p style={{
+              fontSize: '1.5em',
+              fontWeight: 'bold',
+              textAlign: 'center'
+            }}>Categories</p>
+
+            <div className="category-list">
+              {
+                postCategories.map(category => {
+
+                  return (
+
+                    <p key={category.fieldValue} onClick={() => {
+                      this.handleClick(category.fieldValue);
+                    }}>
+                      <FontAwesomeIcon icon={faHandPointRight} />
+                      <span style={{
+                        marginLeft: 5
+                      }}>
+                        {category.fieldValue} ({category.totalCount})
+                                    </span>
+
+                      <br />
+                    </p>
+                  )
+
+                })
+              }
+
+            </div>
+          </div>
+          {/* Sidebar Ends*/}
         </div>
       </Layout>
     )
