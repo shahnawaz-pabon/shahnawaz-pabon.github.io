dada7164a3c76f53dcec0aa701b61ed6b1a4c5ef
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-03-15 23:52:05 +0600

    ThemeProvider added as wrapRootElement into gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,10 @@
 // highlighter: coy,dark,funky,okaidia,solarizedlight,tomorrow,twilight
 require("prismjs/themes/prism-coy.css");
 require("./src/styles/override-prism-coy.css");
+
+// import React from "react";
+// import { ThemeProvider } from "./src/components/ThemeContext";
+
+// export const wrapRootElement = ({ element }) => (
+//   <ThemeProvider>{element}</ThemeProvider>
+// )
