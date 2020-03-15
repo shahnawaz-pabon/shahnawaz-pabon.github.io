79e5b050086c4c44d1f53bbb9bf9a66c75e77853
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-03-15 23:59:25 +0600

    Theme added

---
+++ src/utilities/Theme.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/core";
+
+const themes = {
+  light: {
+    foreground: "inherit",
+    background: "inherit",
+  },
+  dark: {
+    foreground: "#fff",
+    background: "#1f2029",
+  },
+}
