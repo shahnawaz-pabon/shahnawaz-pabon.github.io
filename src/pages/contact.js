1da2b2c42b5b7bceb5779984f8483ce0a98a6c0c
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-03-22 23:47:24 +0600

    Contact form added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -30,6 +30,26 @@ class Contact extends React.Component {
 
           <div className="img">
             <img src={contact} />
+            <div>
+              <form
+                action="https://formspree.io/FORM_ID"
+                method="POST"
+              >
+                <label>
+                  Your Name:
+                  <input type="text" name="name" />
+                </label>
+                <label>
+                  Your Email:
+                  <input type="email" name="_replyto" />
+                </label>
+                <label>
+                  Message:
+                  <textarea name="message"></textarea>
+                </label>
+                <input type="submit" value="Send" />
+              </form>
+            </div>
           </div>
 
 
