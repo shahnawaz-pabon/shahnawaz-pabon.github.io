30a4f4eb75cbf491d00514a8ed5c6c3631a6779a
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-04-01 22:03:03 +0600

    Recent posts title added with all posts linked

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import Helmet from 'react-helmet';
 import config from '../data/config';
 import Layout from '../layout';
@@ -16,7 +16,14 @@ class Index extends React.Component {
         <Slider />
         <div className="container">
           <section>
+            <h1 style={{ textAlign: "center"}}>Recent Posts</h1>
             <PostListing postEdges={postEdges} />
+
+            <h3 style={{
+              textAlign: "end"
+            }}>
+              <Link to="/posts">All Posts</Link>
+            </h3>
           </section>
         </div>
       </Layout>
