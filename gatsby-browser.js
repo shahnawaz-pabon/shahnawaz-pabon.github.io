eb80d7ddea4d644cb6f0005e1c6dd48effd9e4aa
shahnawaz-pabon <s.pabon93@gmail.com>
2021-07-04 01:30:08 +0600

    Updates

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-// highlighter: coy,dark,funky,okaidia,solarizedlight,tomorrow,twilight
-import React from "react";
-import { ThemeProvider } from "./src/components/ThemeContext";
-
-require("./src/styles/syntax-highlighter.css");
-
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>{element}</ThemeProvider>
-)
