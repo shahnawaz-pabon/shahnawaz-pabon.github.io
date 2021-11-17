25b768da4e87b02c56ef7a67ba9616a2248dc818
shahnawaz-pabon <shahnawaz-pabon@users.noreply.github.com>
2021-11-17 18:41:33 +0000

    Deploying to master from @ shahnawaz-pabon/shahnawaz-pabon.github.io@8ee502662ed340933a7d206e02b758b26521499f 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-cfafc36e907307bfe919.js"
+    "url": "webpack-runtime-329396475db51845d8e9.js"
   },
   {
     "url": "styles.1b6866525f307c6e76fa.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "159448896c733d6df3701048a44301c1"
+    "revision": "ad7ed774f1cad99f024627458941d7d6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-77466cc0b3ed4dac08eb.js"
