4b527025ee9f611058c6445fe73c863cf8bac13e
shahnawaz-pabon <s.pabon93@gmail.com>
2023-09-08 19:37:07 +0600

    footer's content updated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import './Footer.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faCopyright } from '@fortawesome/free-solid-svg-icons';
+import React, { Component } from "react";
+import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faCopyright } from "@fortawesome/free-solid-svg-icons";
 
 export default class Footer extends Component {
-
   render() {
+    const currentYear = new Date().getFullYear();
+    const startYear = 2019;
 
     return (
-
       <footer className="footer">
-        <FontAwesomeIcon icon={faCopyright} color="rgb(26, 188, 156)" /> Shahnawaz Hossan 2019-2021 | Crafted with <FontAwesomeIcon icon={faHeart} color="rgb(26, 188, 156)" />
+        <FontAwesomeIcon icon={faCopyright} color="rgb(26, 188, 156)" />{" "}
+        Shahnawaz Hossan {startYear}-{currentYear} | Crafted with{" "}
+        <FontAwesomeIcon icon={faHeart} color="rgb(26, 188, 156)" />
       </footer>
-    )
+    );
   }
 }
