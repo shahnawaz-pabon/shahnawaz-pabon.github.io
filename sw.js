d1790043bd86416cdd7f4c4989decef90df8c54d
shahnawaz-pabon <shahnawaz-pabon@users.noreply.github.com>
2024-09-06 22:09:05 +0000

    Deploying to master from @ shahnawaz-pabon/shahnawaz-pabon.github.io@030c73e28e8d36480a153d98dc616ae9e3da484f 🚀

--- sw.js
+++ sw.js
@@ -27,29 +27,29 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ca9770c878068d13a229.js"
+    "url": "webpack-runtime-27ad472d6ed07d88c5f0.js"
   },
   {
-    "url": "styles.9e7e49fdc71c80671b62.css"
+    "url": "styles.ccdcd626cfd73a7e540b.css"
   },
   {
     "url": "styles-0ec71dd62c66cb95665c.js"
   },
   {
-    "url": "framework-70a2455d4a4320e97528.js"
+    "url": "framework-1b174197fe6013c4312d.js"
   },
   {
-    "url": "app-cdf6668ec8c26631cfb5.js"
+    "url": "app-290e5e869ee2a2425e88.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "afec405cd04a5072f4ed1095ecd7665a"
+    "revision": "a560b86879490c210cabe3b645aee20a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c33574fbc14be9513f80.js"
   },
   {
-    "url": "polyfill-f88fa77a70307660e63d.js"
+    "url": "polyfill-7448e3714f6be6da6c9b.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -145,7 +145,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-cdf6668ec8c26631cfb5.js`))) {
+  if (!resources || !(await caches.match(`/app-290e5e869ee2a2425e88.js`))) {
     return await fetch(event.request)
   }
 
