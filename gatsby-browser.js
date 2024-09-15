35f1c6c0063052ec701f7f6a212b356859fc00fd
shahnawaz-pabon <s.pabon93@gmail.com>
2024-09-15 23:55:20 +0200

    copy to clipboard feature implemented

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,3 +7,26 @@ require("./src/styles/syntax-highlighter.css");
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider>{element}</ThemeProvider>
 )
+
+export const onRouteUpdate = () => {
+  document.querySelectorAll("pre").forEach((codeBlock) => {
+    // Create copy button
+    const copyButton = document.createElement("button");
+    copyButton.innerText = "Copy";
+    copyButton.className = "copy-button";
+
+    // Add click event to copy code
+    copyButton.addEventListener("click", () => {
+      const code = codeBlock.querySelector("code").innerText;
+      navigator.clipboard.writeText(code).then(() => {
+        copyButton.innerText = "Copied!";
+        setTimeout(() => {
+          copyButton.innerText = "Copy";
+        }, 2000);
+      });
+    });
+
+    // Insert copy button before the code block
+    codeBlock.parentNode.insertBefore(copyButton, codeBlock);
+  });
+};
